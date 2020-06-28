397b559f58b15068a823f247df7ee47314fc0892
Leonardo Santos <leonardoluis.santana@hotmail.com>
2020-06-28 20:20:43 -0300

    adicionar controller

---
+++ EFCore.WebApi/Controllers/HeroController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFCore.Domain;
+using EFCore.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EFCore.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class HeroController : ControllerBase
+    {
+        private readonly HeroiContext _context;
+        public HeroController(HeroiContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var listHerois = _context.Herois.ToList();
+            return Ok(listHerois);
+        }
+
+        // GET: api/<controller>
+        [HttpGet("Filtro/{nomeFiltro}")]
+        public ActionResult GetFiltro(string nomeFiltro)
+        {
+            var listHerois = _context.Herois
+                            .Where(h => h.Nome.Contains(nomeFiltro)) 
+                            .ToList();
+
+            return Ok(listHerois);
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{nameHero}")]
+        public ActionResult Get(string nameHero)
+        {
+            var heroi = new Heroi { Nome = nameHero };
+
+            _context.Herois.Add(heroi);
+            _context.SaveChanges();
+
+
+            return Ok();
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
