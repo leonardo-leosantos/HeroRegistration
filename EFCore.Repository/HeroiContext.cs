687f3e44df6d3d23ee3dcba0dad47459342d41b6
Leonardo Santos <leonardoluis.santana@hotmail.com>
2020-06-28 04:03:23 -0300

    adicionar camadas ao projeto

--- EFCore.Repository/HeroiContext.cs
+++ EFCore.Repository/HeroiContext.cs
@@ -1,24 +1,24 @@
-﻿using EFCore.WebApi.Models;
+﻿using EFCore.Domain;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace EFCore.WebApi.Data
+namespace EFCore.Repository
 {
     public class HeroiContext : DbContext
     {
+        public HeroiContext(DbContextOptions<HeroiContext> optionsBuilder) : base(optionsBuilder)
+        {
+
+        }
+
         public DbSet<Heroi> Herois { get; set; }
         public DbSet<Batalha> Batalhas { get; set; }
         public DbSet<Arma> Armas { get; set; }
         public DbSet<HeroiBatalha> HeroisBatalhas { get; set; }
         public DbSet<IdentidadeSecreta> IdentidadesSecretas { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Password=sa123456;Persist Security Info=True;User ID=sa;Initial Catalog=HERO_APP;Data Source=LEOSANT\\SQLEXPRESS");
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
