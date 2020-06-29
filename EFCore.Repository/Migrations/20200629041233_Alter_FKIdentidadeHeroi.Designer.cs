3b38a9abfef3b5f21c361c093bc4347890125b9d
Leonardo Santos <leonardoluis.santana@hotmail.com>
2020-06-29 01:15:09 -0300

    adicionar migration

--- EFCore.Repository/Migrations/20200629041233_Alter_FKIdentidadeHeroi.Designer.cs
+++ EFCore.Repository/Migrations/20200629041233_Alter_FKIdentidadeHeroi.Designer.cs
@@ -4,14 +4,16 @@ using EFCore.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EFCore.Repository.Migrations
 {
     [DbContext(typeof(HeroiContext))]
-    partial class HeroiContextModelSnapshot : ModelSnapshot
+    [Migration("20200629041233_Alter_FKIdentidadeHeroi")]
+    partial class Alter_FKIdentidadeHeroi
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EFCore.Repository/Migrations/20200629041233_Alter_FKIdentidadeHeroi.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCore.Repository.Migrations
+{
+    public partial class Alter_FKIdentidadeHeroi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_IdentidadesSecretas_Herois_HeroiId",
+                table: "IdentidadesSecretas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_IdentidadesSecretas_HeroiId",
+                table: "IdentidadesSecretas");
+
+            migrationBuilder.DropColumn(
+                name: "HeroiId",
+                table: "IdentidadesSecretas");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IdentidadesSecretas_IdHeroi",
+                table: "IdentidadesSecretas",
+                column: "IdHeroi",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_IdentidadesSecretas_Herois_IdHeroi",
+                table: "IdentidadesSecretas",
+                column: "IdHeroi",
+                principalTable: "Herois",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_IdentidadesSecretas_Herois_IdHeroi",
+                table: "IdentidadesSecretas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_IdentidadesSecretas_IdHeroi",
+                table: "IdentidadesSecretas");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "HeroiId",
+                table: "IdentidadesSecretas",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IdentidadesSecretas_HeroiId",
+                table: "IdentidadesSecretas",
+                column: "HeroiId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_IdentidadesSecretas_Herois_HeroiId",
+                table: "IdentidadesSecretas",
+                column: "HeroiId",
+                principalTable: "Herois",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
